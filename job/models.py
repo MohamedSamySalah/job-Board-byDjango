384b1bb62d52c27558ba22d33bfb768cdaf85432
Mohamedsamy <201801746@pua.edu.eg>
2022-11-12 19:32:09 +0200

    i create jobs model and update a last version

--- job/models.py
+++ job/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Jobs(models.Model):
+    fulltime = 'FT'
+    parttime = 'PT'
+    job_type_choices = [
+        (fulltime,'Fulltime'),
+        (parttime,'Parttime'),
+    ]
+    title = models.CharField(max_length=100)
+    # Location
+    job_type = models.CharField(max_length=2,choices=job_type_choices,default=fulltime)
+    description = models.TextField(max_length=500)
+    pulished_at = models.DateTimeField(auto_now=True)
+    vecancy = models.IntegerField(default = 1)
+    salary = models.DecimalField(decimal_places=3,max_digits=8,default=1.1)
+    experience = models.IntegerField(default=1)
+
+    def __str__(self) :
+        return self.title
