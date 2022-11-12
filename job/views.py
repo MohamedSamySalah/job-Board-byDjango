2cdbada669dc1211057ddcf19823903948061dbc
Mohamedsamy <201801746@pua.edu.eg>
2022-11-12 22:25:50 +0200

    uploaded views of job app and urls

--- job/views.py
+++ job/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from .models import Jobs,Category
 # Create your views here.
 
 
 def all_jobs(request):
-    pass
+    job_list = Jobs.objects.all()
+    category_list = Category.objects.all()
+    context = {
+        'jobs': job_list,
+        'job_cat': category_list
+    }
+    return render(request,'alljobs.html',context)
 
-def job_detalis(request, id):
-    pass
+def job_detalis(request,id):
+    job = Jobs.objects.get(id = id)
+    context={
+        'job': job
+    }
+    return render(request,'job_details.html',context)
